ac5963d97034fc28f96d97661b6b41e7207f5841
Batuhan Zorbey Zengin <batuhanzorbeyzengin@gmail.com>
2023-01-04 02:26:58 +0300

    dev

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function Layout({children, title, description}) {
+export default function Layout({ children, title, description }) {
 
     const router = useRouter();
 
@@ -14,11 +14,12 @@ export default function Layout({children, title, description}) {
             router.push("/login");
         }
     };
-    return(
+    return (
         <>
             <Head>
                 <title>{title}</title>
                 <meta name="description" content={description} />
+                <meta name="robots" content="noindex" />
             </Head>
             {/* <!-- Page Wrapper --> */}
             <div id="wrapper">
@@ -45,27 +46,6 @@ export default function Layout({children, title, description}) {
                     <div className="sidebar-heading">
                         Articles
                     </div>
-
-                    {/* <!-- Nav Item - Pages Collapse Menu --> */}
-                    {/* <li className="nav-item">
-                        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                            aria-expanded="true" aria-controls="collapsePages">
-                            <i className="fas fa-fw fa-folder"></i>
-                            <span>Pages</span>
-                        </a>
-                        <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                            <div className="bg-white py-2 collapse-inner rounded">
-                                <h6 className="collapse-header">Login Screens:</h6>
-                                <a className="collapse-item" href="login.html">Login</a>
-                                <a className="collapse-item" href="register.html">Register</a>
-                                <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                                <div className="collapse-divider"></div>
-                                <h6 className="collapse-header">Other Pages:</h6>
-                                <a className="collapse-item" href="404.html">404 Page</a>
-                                <a className="collapse-item" href="blank.html">Blank Page</a>
-                            </div>
-                        </div>
-                    </li> */}
 
                     {/* <!-- Nav Item - Charts --> */}
                     <li className="nav-item">
@@ -168,33 +148,8 @@ export default function Layout({children, title, description}) {
                                 <div className="topbar-divider d-none d-sm-block"></div>
 
                                 {/* <!-- Nav Item - User Information --> */}
-                                <li className="nav-item dropdown no-arrow">
-                                    <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            {/* TODO: user profile picture and name will be retrieved from service */}
-                                        <span className="mr-2 d-none d-lg-inline text-gray-600 small">Batuhan Zorbey Zengin</span>
-                                    </a>
-                                    {/* <!-- Dropdown - User Information --> */}
-                                    <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                        aria-labelledby="userDropdown">
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Profile
-                                        </a>
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Settings
-                                        </a>
-                                        <a className="dropdown-item" href="#">
-                                            <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Activity Log
-                                        </a>
-                                        <div className="dropdown-divider"></div>
-                                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                            <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                            Logout
-                                        </a>
-                                    </div>
+                                <li className="nav-item dropdown no-arrow d-flex align-items-center">
+                                    <span className="mr-2 d-none d-lg-inline text-gray-600 small">Batuhan Zorbey Zengin</span>
                                 </li>
 
                             </ul>
